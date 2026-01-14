51bef6333bd78ced1d4bd3cbce91a9f75939ce0d
EngelAlexey <alexhmanzanares@gmail.com>
2026-01-14 17:19:40 -0600

    Correct `pinia-plugin-persistedstate` module import path in Nuxt config and delete `CONTRIBUTING.md`.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  srcDir: 'app',
-  css: ['~/styles/main.css'],
-
   modules: [
     '@nuxtjs/supabase',
     '@nuxt/ui',
     '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/i18n'
   ],
 
@@ -26,6 +21,30 @@ export default defineNuxtConfig({
       'composables/utils'
     ]
   },
+  devtools: { enabled: true },
+  css: ['~/styles/main.css'],
+
+  runtimeConfig: {
+    supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+    onvoSecretKey: process.env.ONVO_WEBHOOK_SECRET,
+    onvoWebhookSecret: process.env.ONVO_REAL_WEBHOOK_SECRET,
+    onvoPriceStartup: process.env.NUXT_ONVO_PRICE_STARTUP,
+
+    stripePriceSolo: process.env.STRIPE_PRICE_SOLO,
+    stripePriceStartup: process.env.STRIPE_PRICE_STARTUP,
+
+    public: {
+      siteUrl: process.env.SITE_URL,
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+      stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      onvoPublishableKey: process.env.ONVO_API_KEY
+    }
+  },
+  srcDir: 'app',
+  compatibilityDate: '2025-07-15',
 
   i18n: {
     langDir: 'locales',
@@ -48,7 +67,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
-      exclude: ['/', '/register', '/pricing', '/payment/processing', '/payment/test', '/confirm'],
+      exclude: ['/', '/register', '/pricing', '/payment/processing', '/payment/test', '/confirm']
     },
 
     cookieOptions: {
@@ -64,26 +83,6 @@ export default defineNuxtConfig({
         persistSession: true,
         autoRefreshToken: true
       }
-    }
-  },
-
-  runtimeConfig: {
-    supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
-    onvoSecretKey: process.env.ONVO_WEBHOOK_SECRET,
-    onvoWebhookSecret: process.env.ONVO_REAL_WEBHOOK_SECRET,
-    onvoPriceStartup: process.env.NUXT_ONVO_PRICE_STARTUP,
-
-    stripePriceSolo: process.env.STRIPE_PRICE_SOLO,
-    stripePriceStartup: process.env.STRIPE_PRICE_STARTUP,
-
-    public: {
-      siteUrl: process.env.SITE_URL,
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY,
-      stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-      onvoPublishableKey: process.env.ONVO_API_KEY
     }
   }
 })
